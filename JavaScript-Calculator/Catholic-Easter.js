cbe4776d1c0d0e3d491aa5eb69aadb57268f2346
AlexeiCrystal <alex211952i@gmail.com>
2025-04-18 20:24:14 +0300

    Added Catholic Easter Calculator (JavaScript)

---
+++ JavaScript-Calculator/Catholic-Easter.js
@@ -0,0 +1,23 @@
+// Code by github.com/AlexeiCrystal
+function calculateCatholicEaster(year) {
+    const a = year % 19;
+    const b = Math.floor(year / 100);
+    const c = year % 100;
+    const d = Math.floor(b / 4);
+    const e = b % 4;
+    const f = Math.floor((b + 8) / 25);
+    const g = Math.floor((b - f + 1) / 3);
+    const h = (19 * a + b - d - g + 15) % 30;
+    const i = Math.floor(c / 4);
+    const k = c % 4;
+    const l = (32 + 2 * e + 2 * i - h - k) % 7;
+    const m = Math.floor((a + 11 * h + 22 * l) / 451);
+    const month = Math.floor((h + l - 7 * m + 114) / 31);
+    const day = ((h + l - 7 * m + 114) % 31) + 1;
+    const date = new Date(year, month - 1, day);
+    const finalDay = date.getDate();
+    const finalMonth = date.getMonth() + 1; 
+    return `${finalDay}.${finalMonth}`;
+}
+console.log(calculateCatholicEaster(2025))
+//Input 2025; Output: 20.4
