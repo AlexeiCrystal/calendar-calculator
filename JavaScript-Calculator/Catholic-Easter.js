44e9ea83ed701712a27ad4c7fb7bfdeeb564765e
AlexeiCrystal <alex211952i@gmail.com>
2025-04-19 11:23:47 +0300

    Update Catholic-Easter.js

--- JavaScript-Calculator/Catholic-Easter.js
+++ JavaScript-Calculator/Catholic-Easter.js
@@ -1,23 +1,41 @@
 // Code by github.com/AlexeiCrystal
-function calculateCatholicEaster(year) {
-    const a = year % 19;
-    const b = Math.floor(year / 100);
-    const c = year % 100;
-    const d = Math.floor(b / 4);
-    const e = b % 4;
-    const f = Math.floor((b + 8) / 25);
-    const g = Math.floor((b - f + 1) / 3);
-    const h = (19 * a + b - d - g + 15) % 30;
-    const i = Math.floor(c / 4);
-    const k = c % 4;
-    const l = (32 + 2 * e + 2 * i - h - k) % 7;
-    const m = Math.floor((a + 11 * h + 22 * l) / 451);
-    const month = Math.floor((h + l - 7 * m + 114) / 31);
-    const day = ((h + l - 7 * m + 114) % 31) + 1;
-    const date = new Date(year, month - 1, day);
-    const finalDay = date.getDate();
-    const finalMonth = date.getMonth() + 1; 
-    return `${finalDay}.${finalMonth}`;
+function calculateCatholicEaster(CatholicEasterCalculateYear) {
+    const mod19Result = CatholicEasterCalculateYear % 19;
+    const centuryQuotient = Math.floor(CatholicEasterCalculateYear / 100);
+    const yearInCentury = CatholicEasterCalculateYear % 100;
+    const centuryDiv4 = Math.floor(centuryQuotient / 4);
+    const centuryMod4 = centuryQuotient % 4;
+    const centuryAdjust = Math.floor((centuryQuotient + 8) / 25);
+    const centuryAdjust2 = Math.floor((centuryQuotient - centuryAdjust + 1) / 3);
+    const paschalFullMoonOffset = (19 * mod19Result + centuryQuotient - centuryDiv4 - centuryAdjust2 + 15) % 30;
+    const yearInCenturyDiv4 = Math.floor(yearInCentury / 4);
+    const yearInCenturyMod4 = yearInCentury % 4;
+    const sundayOffset = (32 + 2 * centuryMod4 + 2 * yearInCenturyDiv4 - paschalFullMoonOffset - yearInCenturyMod4) % 7;
+    const paschalAdjustment = Math.floor((mod19Result + 11 * paschalFullMoonOffset + 22 * sundayOffset) / 451);
+    const computedMonth = Math.floor((paschalFullMoonOffset + sundayOffset - 7 * paschalAdjustment + 114) / 31);
+    const computedDay = ((paschalFullMoonOffset + sundayOffset - 7 * paschalAdjustment + 114) % 31) + 1;
+    const tempDate = new Date(CatholicEasterCalculateYear, computedMonth - 1, computedDay);
+    const adjustedDay = tempDate.getDate();
+    const adjustedMonth = tempDate.getMonth() + 1; 
+    return `${adjustedDay}.${adjustedMonth}`;
 }
-console.log(calculateCatholicEaster(2025))
-//Input 2025; Output: 20.4
+
+function getCatholicEasterDay(CatholicEasterCalculateYear) {
+    const catholicDateResult = calculateCatholicEaster(CatholicEasterCalculateYear);
+    return parseInt(catholicDateResult.split('.')[0], 10);
+}
+
+function getCatholicEasterMonth(CatholicEasterCalculateYear) {
+    const catholicDateResult = calculateCatholicEaster(CatholicEasterCalculateYear);
+    return parseInt(catholicDateResult.split('.')[1], 10);
+}
+
+// Example
+console.log(calculateCatholicEaster(2025));
+console.log('\n');
+//Input 2025; Output 20.4
+console.log(getCatholicEasterDay(2025));    
+console.log('\n');
+// Input 2025; Output 20
+console.log(getCatholicEasterMonth(2025));  
+// Input 2025; Output 4
